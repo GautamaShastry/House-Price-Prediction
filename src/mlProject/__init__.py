ef209b0cd213e156bd118e6320c27999e83af54e
gautamasb <gautamasastryb@backflipt.com>
2024-07-04 15:01:50 -0500

    Logging implementation done

--- src/mlProject/__init__.py
+++ src/mlProject/__init__.py
@@ -1,20 +1,22 @@
+# Provides log messages in both files and console
+
 import os
 import sys
 import logging
 
-logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"  # log message string
 
-logging_dir = "logs"
-log_filepath = os.path.join(logging_dir, "running_logs.log")
-os.makedirs(logging_dir, exist_ok=True)
+logging_dir = "logs"  # directory where log messages are stored
+log_filepath = os.path.join(logging_dir, "running_logs.log") # file where log messages are stored
+os.makedirs(logging_dir, exist_ok=True) # create the log directory 
 
 logging.basicConfig(
-    filename=log_filepath, 
     level=logging.INFO, 
     format=logging_str,
     handlers=[
-        logging.FileHandler(log_filepath),
-        logging.StreamHandler(sys.stdout)
+        logging.FileHandler(log_filepath), # log messages in file
+        logging.StreamHandler(sys.stdout) # log messages in console
     ]
 )
 
+logger = logging.getLogger("mlProjectLogger")
