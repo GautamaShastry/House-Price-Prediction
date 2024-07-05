36fa0e1bc3ff9c271e9fce81833ff9dc21c0203d
gautamasb <gautamasastryb@backflipt.com>
2024-07-05 17:51:52 -0500

    validation completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01 import DataTrainingPipeline
+from mlProject.pipeline.stage_02 import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -7,6 +8,17 @@ STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f">>>>>> starting {STAGE_NAME} <<<<<<")
     data = DataTrainingPipeline()
+    data.main()
+    logger.info(f"<<<<<<  {STAGE_NAME} completed >>>>>>")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+    logger.info(f">>>>>> starting {STAGE_NAME} <<<<<<")
+    data = DataValidationTrainingPipeline()
     data.main()
     logger.info(f"<<<<<<  {STAGE_NAME} completed >>>>>>")
 except Exception as e:
